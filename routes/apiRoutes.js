729a706e964bfb575ee483909daa80fcde3dd265
Jamie Epstein <jasepstein@gmail.com>
2019-07-01 14:42:43 -0500

    WIP staging

---
+++ routes/apiRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+module.exports = (
+    //Request to get the Books API 
+    app.get('/api/books', (req, res) => {
+        res.render()
+    })
+
+    //Request to post the to Books API 
+
+    //Request to get a specific book based on URL passed in
+    
+
+
+);
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require('mongoose');
+const routes = require('./routes/apiRoutes')
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+app.use('/api', routes);
+
+//Connect to Mongoose Server
+mongoose.connect(
+  'mongodb://localhost:27017/googleBooksSearch-React',
+  {useNewUrlParser: true}
+)
+.then(console.log(() => "Connected to MongoDB!"))
+.catch(err => console.log(err));
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
